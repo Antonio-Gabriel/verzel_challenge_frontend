9f21f7131cd362743329a4f10477f7c4b6bcd7b4
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-16 03:05:22 +0100

    axios config

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,8 @@ import axios from "axios";
 // Actualizar o host apartir daqui
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/"
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+  headers: {
+    "Content-type": "application/json",
+  },
 });
